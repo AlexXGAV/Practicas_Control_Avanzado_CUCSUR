f69f4d6055bd3430cfebe1baa0953c7626fe81da
Alex <106277349+AlexXGAV@users.noreply.github.com>
2023-10-18 12:40:29 -0600

    Update capture_serial_port.py

--- Respuesta_al_impulso/capture_serial_port.py
+++ Respuesta_al_impulso/capture_serial_port.py
@@ -1,9 +1,9 @@
 import serial
 import csv
 
-arduino_port = "COM3"           # serial port of Arduino
+arduino_port = "COM16"           # serial port of Arduino
 baud = 9600                     # arduino uno runs at 9600 baud
-fileName="data.csv"             # name of the CSV file generated
+fileName = "data.csv"            # name of the CSV file generated
 ser = serial.Serial(arduino_port, baud)
 print("Connected to port:" + arduino_port)
 
@@ -11,7 +11,7 @@ TIME_CAPTURED_MS = 30000        # total time to capture
 DELAY_BETWEEN_SAMPLES = 13
 
 # how many samples to collect
-SAMPLES = TIME_CAPTURED_MS/DELAY_BETWEEN_SAMPLES
+SAMPLES = int(TIME_CAPTURED_MS / DELAY_BETWEEN_SAMPLES)
 
 line = 0
 data = []                       # store data
@@ -19,16 +19,11 @@ data = []                       # store data
 # collect the samples
 with open(fileName, "a", encoding='UTF8') as file:
     print("Created file")
-    while line <= SAMPLES:
-        getData=ser.readline()
-        dataString = getData.decode('utf-8')
-        data=dataString[0:][:-2]
-        #print(data)
-        readings = data.split(",")
-        #print(readings)
+    while line < SAMPLES:
+        getData = ser.readline()
+        dataString = getData.decode('utf-8').strip()  # Remove trailing newline character
+        readings = dataString.split(",")
         data.append(readings)
-        #print(data)
-
         line += 1
 
 # create the CSV
@@ -37,5 +32,4 @@ with open(fileName, 'w', encoding='UTF8', newline='') as f:
     writer.writerows(data)
 
 print("Data collection complete!")
-file.close()
 ser.close()
